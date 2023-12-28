aac9d9cbc4aa1ad46e29a9628a7a160421f3e253
Kim DoHyeon <dohyun51731@gmail.com>
2023-12-28 16:38:20 +0900

    앨범 앱 main, screen 나누기

---
+++ lib/album_screen/album_app_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AlbumAppScreen extends StatefulWidget {
+  const AlbumAppScreen({super.key});
+
+  @override
+  State<AlbumAppScreen> createState() => _AlbumAppScreenState();
+}
+
+class _AlbumAppScreenState extends State<AlbumAppScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
