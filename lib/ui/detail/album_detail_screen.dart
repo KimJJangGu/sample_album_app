f4959d19920bb40807534cdf6b0b8735d88391e2
Kim DoHyeon <dohyun51731@gmail.com>
2023-12-29 23:54:00 +0900

    ViewModel 추가

--- lib/ui/detail/album_detail_screen.dart
+++ lib/ui/detail/album_detail_screen.dart
@@ -1,18 +1,30 @@
+import 'package:album_app/model/photo.dart';
 import 'package:flutter/material.dart';
+import '../../repository/album_repository.dart';
+import '../../repository/album_repository_impl.dart';
 
 class AlbumDetailScreen extends StatelessWidget {
   final String albumId;
+  final AlbumRepository repository = AlbumRepositoryImpl();
 
-  const AlbumDetailScreen({super.key, required this.albumId});
+  AlbumDetailScreen({super.key, required this.albumId});
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('사진?'),
         titleTextStyle: const TextStyle(color: Colors.white, fontSize: 16),
         backgroundColor: Colors.blue,
+      ),
+      body: FutureBuilder(
+        future: repository.getPhoto(albumId),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            final List<Photo> albumList = snapshot.data!;
+          }
+          return Text('fsd');
+        },
       ),
     );
   }
